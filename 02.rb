f7097801f0c709a8ef177ea566d17c1a72d31f5f
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2019-12-04 23:30:47 -0800

    Quick commit day 5 part 1

--- 02.rb
+++ 02.rb
@@ -6,8 +6,11 @@ part_1_input = input.clone
 part_1_input[1] = 12
 part_1_input[2] = 2
 
-puts "Part 1: #{IntcodeComputer.new.run(part_1_input)}"
-puts "Part 2: #{IntcodeProgramInputSeeker.new.seek(program: input, goal: 19690720, noun_range: (0..99), verb_range: (0..99)).join}"
+puts "Part 0: #{IntcodeProgram.new("1,0,0,0,99".split(',').map(&:to_i)).run}"
+puts "Part 0: #{IntcodeProgram.new("1,1,1,4,99,5,6,0,99".split(',').map(&:to_i)).run}"
+
+puts "Part 1: #{IntcodeProgram.new(part_1_input).run}"
+#puts "Part 2: #{IntcodeProgramInputSeeker.new.seek(program: input, goal: 19690720, noun_range: (0..99), verb_range: (0..99)).join}"
 
 __END__
 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,2,23,10,27,1,27,5,31,1,31,6,35,1,6,35,39,2,39,13,43,1,9,43,47,2,9,47,51,1,51,6,55,2,55,10,59,1,59,5,63,2,10,63,67,2,9,67,71,1,71,5,75,2,10,75,79,1,79,6,83,2,10,83,87,1,5,87,91,2,9,91,95,1,95,5,99,1,99,2,103,1,103,13,0,99,2,14,0,0
--- intcode_computer.rb
+++ intcode_computer.rb
@@ -4,32 +4,90 @@ class IntcodeProgram
   # INTCODEs
   ADD = 1
   MULTIPLY = 2
+  INPUT = 3
+  OUTPUT = 4
   EXIT = 99
 
-  def run(memory)
-    i = 0
+  attr_accessor :memory, :i
 
+  def initialize(memory)
+    @memory = memory
+    @pointer = 0
+  end
+
+  def run
     loop do
-      noun_pos = memory[i + 1]
-      verb_pos = memory[i + 2]
-      output_pos = memory[i + 3]
-      noun = memory[noun_pos]
-      verb = memory[verb_pos]
+      intcode = @memory[@pointer]
 
-      case memory[i]
-      when ADD
-        memory[output_pos] = noun + verb
-      when MULTIPLY
-        memory[output_pos] = noun * verb
-      when EXIT
-        return memory[0]
-      else
-        raise "Unknown INTCODE #{memory[i]}"
+      if intcode.digits.size > 2
+        intcode = intcode.digits.reverse.last(2).map(&:to_s).join.to_i
       end
 
-      i += 4
+      if intcode == ADD
+        add(*@memory[@pointer, 4])
+      elsif intcode == MULTIPLY
+        multiply(*@memory[@pointer, 4])
+      elsif intcode == INPUT
+        input(@memory[@pointer + 1])
+      elsif intcode == OUTPUT
+        output(@memory[@pointer + 1])
+      elsif EXIT
+        break
+      else
+        raise "Unknown INTCODE #{intcode}"
+      end
+      puts @memory.join(',')
     end
 
-    nil
+    @memory[0]
+  end
+
+  def add(mode, input_1, input_2, output)
+    digits = mode.to_s.rjust(5, '0').chars.map(&:to_i)
+
+    a = if digits[2] == 0
+          @memory[input_1]
+        else
+          input_1
+        end
+
+    b = if digits[1] == 0
+          @memory[input_2]
+        else
+          input_2
+        end
+
+    @memory[output] = a + b
+    @pointer += 4
+  end
+
+  def multiply(mode, input_1, input_2, output)
+    digits = mode.to_s.rjust(5, '0').chars.map(&:to_i)
+
+    a = if digits[2] == 0
+          @memory[input_1]
+        else
+          input_1
+        end
+
+    b = if digits[1] == 0
+          @memory[input_2]
+        else
+          input_2
+        end
+
+    @memory[output] = a * b
+    @pointer += 4
+  end
+  
+  def input(address)
+    print "Feed me: "
+    @memory[address] = gets.chomp.to_i
+    @pointer += 2
+  end
+
+  def output(address)
+    puts(@memory[address])
+    @pointer += 2
   end
 end
